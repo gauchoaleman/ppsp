0d6c953a2bbf604c13d6c72428939f9755b3be06
Stefan Ronacher <stefan.ronacher.78@gmail.com>
2020-10-31 11:44:30 -0300

    Trying to get ready frontend menus

--- resources/views/includes/front/top_bar/text_menus/level1/level2/level3.blade.php
+++ resources/views/includes/front/top_bar/text_menus/level1/level2/level3.blade.php
@@ -4,14 +4,15 @@ use App\Classes\Config\Menu\Level3\Level3;
 $level3 = new Level3;
 $menu_level3_items = $level2->get_menu_items($menu_level2_item_id);
 ?>
-@if( sizeof($menu_level3_items))
 <ul class="dropdown-menu">
-  @foreach($menu_level3_items as $menu_level3_item )
-    <li>
-      <a href="/configuration/load_paper_prices">
-        {{$menu_level3_item->menu_text}}
-      </a>
-    </li>
-  @endforeach
+  <li>
+    <a href="/configuration/load_leaf_sizes">
+      Cargar
+    </a>
+  </li>
+  <li>
+    <a href="/configuration/show_leaf_sizes">
+      Mostrar
+    </a>
+  </li>
 </ul>
-@endif
